5b63af57549c4464e828db7a05b6e5f1836f96bf
Islam Madkaur <islammadkaur@gmail.com>
2020-10-24 23:43:14 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,18 +1,49 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
+  index = 0
+  coupons.each do |coupon|
+    item_coupon = find_item_by_name_in_collection(coupon[:item], cart)
+    item_in_basket = !!item_coupon
+    big_count = item_in_basket && item_coupon[:count] >= coupon[:num]
+    if item_in_basket and big_count
+      cart << { item: "#{item_coupon[:item]} W/COUPON",
+                price: coupon[:cost] / coupon[:num],
+                clearance: item_coupon[:clearance],
+                count: coupon[:num]
+              }
+              item_coupon[:count] -= coupon[:num]
+            end
+            index += 1
+          end
+          cart
+        end
+  # Consult README for inputs and outputs
+  #
+  # REMEMBER: This method **should** update cart
+
+def apply_clearance(cart)
+  cart.map do |item|
+    if item[:clearance]
+      item[:price] *= 0.8
+    end
+    item
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
 end
-
-def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
 end
 
 def checkout(cart, coupons)
+  final_price = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  empty_cart = 0
+  final_price.each do |item|
+    empty_cart += item[:price] * item[:count]
+  end
+
+  empty_cart *= 0.9 if empty_cart > 100
+  empty_cart.round(2)
+
   # Consult README for inputs and outputs
   #
   # This method should call
